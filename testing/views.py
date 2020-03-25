2a08007affa19219af02ef3e17d36e5726d39121
nhanpotter <nhanpotter8a@gmail.com>
2020-03-25 13:17:38 +0800

    Add API Check Connection for testing

--- testing/views.py
+++ testing/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import User, Group
 from rest_framework.authentication import TokenAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from .serializers import UserSerializer, GroupSerializer
 
 
@@ -21,3 +23,12 @@ class GroupViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication, BasicAuthentication]
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+class TestingView(APIView):
+    permission_classes = (AllowAny,)
+
+    def get(self, request, format=None):
+        content = {
+            'connection': True
+        }
+        return Response(content)

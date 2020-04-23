0fed61e2543530fdedaa2c456c8f8b095c7aaf1f
nhanpotter <nhanpotter@gmail.com>
2020-04-23 18:08:34 +0800

    Adjust Integration Test

--- client/test/integration/test_integration.gd
+++ client/test/integration/test_integration.gd
@@ -50,9 +50,9 @@ func test_integration_common_and_network():
 		"Check integration between CommonServices and Network_Services")
 	
 
-func test_integration_expedition_and_network():
+func test_integration_expedition_and_network_test_1():
 	"""
-	Test Integration between Expedition and Network Subsystems
+	Test Integration between Expedition and Network Subsystems on getting expedition list
 	"""
 	yield(self, 'login_success')
 	Network_Services.token = _token
@@ -63,18 +63,28 @@ func test_integration_expedition_and_network():
 	assert_ne(len(Expedition_Lobby_Manager.expedition_list), 0, 
 		"Check integration between Expedition_Lobby_Manager and Network_Services")
 		
+
+func test_integration_expedition_and_network_test_2():
+	"""
+	Test Integration between Expedition and Network Subsystems on getting data of specific expedition
+	"""
+	yield(self, 'login_success')
+	Network_Services.token = _token
+
+	Expedition_Lobby_Manager.test_request_expedition_list()
+	yield(Expedition_Lobby_Manager, 'get_lobby_success')
+
 	var first_expedition = Expedition_Lobby_Manager.expedition_list[0]
 	Expedition_Manager.test_request_worlds(first_expedition.id)
 	yield(Expedition_Manager, 'get_expedition_success')
-#
+
 	assert_ne(len(Expedition_Manager.worlds), 0, 
 		"Check integration between Expedition Lobby and Network_Services")
 
-	
 
 func test_integration_world_and_network():
 	"""
-	Test Integration between Expedition and Network Subsystems
+	Test Integration between World and Network Subsystems
 	"""
 	yield(self, 'login_success')
 	Network_Services.token = _token
@@ -95,9 +105,9 @@ func test_integration_world_and_network():
 		"Check Integration between World_Manager and Network_Services")
 
 
-func test_integration_expeditition_common_and_network():
+func test_integration_common_and_battle():
 	"""
-	Test Integration between Common and Network Subsystems
+	Test Integration between Common and Battle Subsystems
 	"""
 	yield(self, 'login_success')
 	Network_Services.token = _token
@@ -106,10 +116,10 @@ func test_integration_expeditition_common_and_network():
 	yield(Common_Services, 'get_avatar_success')
 
 	assert_not_null(Common_Services.get_user_stats(), 
-		"Check integration between CommonServices and Network_Services")
+		"Check integration between Common_Services and Network_Services")
 
 	Battle_Manager.test_init_battle()
 
 	assert_ne(Battle_Manager.get_self_stats(), {},
-		"Check integration between CommonServices and Network_Services")
+		"Check integration between Common_Services and Battle_Manager")
 			
